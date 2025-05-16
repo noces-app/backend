6690ac6afa60c21d837f97d3cc5992e46a479262
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-16 16:21:00 +0200

    [refactor] remove unused UsersController from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
 
@@ -8,7 +7,6 @@ import { User, UserSchema } from './schemas/user.schema';
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
